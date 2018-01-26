c53a0e8b4ced3b7042a3e7b506a5ee85c2058c13
yakovlev <ivanvyakovlev@gmail.com>
2018-01-26 18:37:04 +0300

    init

--- build/bundle.js
+++ build/bundle.js
@@ -306,7 +306,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
 
-var PORT = 3000;
+var PORT = process.ENV.PORT || 3000;
 var app = (0, _express2.default)();
 
 app.use((0, _compression2.default)());
